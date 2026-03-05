4a70e98bc199387bff235f108a53abd129080215
dmgonzalezhenao <dmgh20212022@gmail.com>
2026-03-05 15:59:50 -0500

    feat(api): implement GET endpoints for log retrieval
    Expose log retrieval endpoints with support for filtering and pagination.
    
    Integrate CRUD operations with FastAPI path and query parameters.
    
    Add LogResponse validation for consistent API output.

--- app/main.py
+++ app/main.py
@@ -8,13 +8,13 @@ for log ingestion and system health monitoring.
 from fastapi import FastAPI, Depends, Body, Path, Query
 
 # Import LogCreate object to get log data schema
-from app.schemas.log_schemas import LogCreate, LogResponse
+from app.schemas.log_schemas import LogLevel, LogCreate, LogResponse
 
 # Import function to create a database session
 from app.database.config import get_db
 
 # Import CRUD logic to save logs in the database
-from app.crud.log_crud import create_log as crud_save_log
+from app.crud.log_crud import create_log as crud_save_log, get_logs as crud_get_logs, get_logs_by_id as crud_get_logs_by_id
 
 # Import types for static analysis and type hinting
 from datetime import datetime, timezone
@@ -62,3 +62,32 @@ async def ingest_log(
     
     # Persist the validated log and return the database record
     return crud_save_log(db=db, log_data=log)
+
+@app.get("/v1/logs", tags=["Retrieval"], response_model=list[LogResponse])
+async def read_logs(
+    # Create connection to database
+    db: Session = Depends(get_db),
+
+    # Get filters by service, level and limit the data
+    service_name: Annotated[str | None, Query(max_length=50)] = None,
+    log_level: Annotated[LogLevel | None, Query()] = None,
+    limit: Annotated[int, Query(ge=1, le=100)] = 10
+):
+    """
+    Retrieve a filtered list of logs from the database.
+
+    This endpoint allows for searching logs based on the originating service 
+    name and the severity level. It supports pagination through a limit 
+    parameter to ensure optimal performance.
+
+    Args:
+        service_name (str, optional): The name of the service that generated the log.
+        log_level (str, optional): The severity level of the log (e.g., INFO, ERROR).
+        limit (int): The maximum number of log records to return (Default: 10, Max: 100).
+
+    Returns:
+        list[LogResponse]: A list of log records matching the criteria.
+    """
+
+    # Return the logs list with get logs function
+    return crud_get_logs(db, service_name=service_name, log_level=log_level, limit=limit)
