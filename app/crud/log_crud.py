ac90cb1b68c1e16be5947da1a181fdc1b3b329f2
dmgonzalezhenao <dmgh20212022@gmail.com>
2026-03-09 12:31:58 -0500

    feat: integrate full observability in CRUD operations
    Add persistence logging for log creation.
    
    Implement conditional logging for successful and failed lookups by ID.
    
    Log query parameters and result counts for bulk retrieval.

--- app/crud/log_crud.py
+++ app/crud/log_crud.py
@@ -15,6 +15,9 @@ from app.database.models import Log
 # Import Pydantic schema for logs input and output
 from app.schemas.log_schemas import LogCreate, LogResponse
 
+# Import Sentinel's logger
+from app.core.logger import logger
+
 def create_log(db: Session, log_data: LogCreate) -> Log:
     """
     Function to create a SQLAlchemy Log object using the data
@@ -24,6 +27,9 @@ def create_log(db: Session, log_data: LogCreate) -> Log:
     db_log = Log(**log_data.model_dump()) 
 
     try:
+        # Log attempt to insert data in database
+        logger.info(f"CRUD: Attempting to persist log for {log_data.service_name}")
+
         # Prepare data insertion in the database
         # Useful if something goes wrong, don't add anything to the database
         db.add(db_log)
@@ -34,11 +40,17 @@ def create_log(db: Session, log_data: LogCreate) -> Log:
         # Get the log with its id and new timestamp
         db.refresh(db_log)
 
+        # Log succesful log redords
+        logger.info(f"CRUD: Log record created successfully with ID: {db_log.id}")
+
         # Return object with new data
         return db_log
     
     # Exception if connection fails
     except Exception as e:
+        # Log error in Sentinel's logger
+        logger.error(f"CRUD: Failed to create log entry: {str(e)}")
+
         # Avoid every change to the database to
         # avoid errors or corrupt data insertions
         db.rollback()
@@ -55,6 +67,8 @@ def get_logs(
     """
     Retrieve a collection of logs with optional filtering and pagination.
     """
+    # Log database consult
+    logger.info(f"CRUD: Fetching logs with filters -> service: {service_name}, level: {log_level}, limit: {limit}")
 
     # Prepares query for the database (SELECT * FROM logs)
     query = db.query(Log)
@@ -70,7 +84,13 @@ def get_logs(
         query = query.filter(Log.log_level == log_level)
 
     # Return query to the database with the limit
-    return query.limit(limit).all()
+    results = query.limit(limit).all()
+
+    # Log result in Sentinel's logger
+    logger.info(f"CRUD: Successfully retrieved {len(results)} log records.")
+
+    # Return result
+    return results
 
 def get_logs_by_id(
     db: Session,
@@ -79,4 +99,14 @@ def get_logs_by_id(
     """
     Retrieve a specific log by its id.
     """
-    return db.query(Log).filter(Log.id == log_id).first()
+    # Make query to database
+    result = db.query(Log).filter(Log.id == log_id).first()
+
+    # Check result and log succesful retrieve or failure
+    if result:
+        logger.info(f"CRUD: Successfully retrieved log record with ID: {log_id}")
+    else:
+        logger.warning(f"CRUD: Log record with ID {log_id} was not found")
+
+    # Return log
+    return result
