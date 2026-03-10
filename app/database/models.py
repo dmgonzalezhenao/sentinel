8b363fc8afdbe87e3e6ba11e4f3fd392784be8fc
dmgonzalezhenao <dmgh20212022@gmail.com>
2026-03-09 22:44:36 -0500

    feat: implement User model and establish relationship with Log table
    Create User class with username, email, and hashed_password fields.
    
    Add created_at and updated_at timestamps for auditing purposes.
    
    Implement user_id foreign key in Log table to track log ownership.
    
    Use JSONB for flexible log metadata storage.

--- app/database/models.py
+++ app/database/models.py
@@ -12,15 +12,27 @@ Table: logs
 | id           | Integer   | PK, Autoincrement            |
 | service_name | String    | Indexed, e.g. 'web-app'      |
 | log_level    | String    | INFO, ERROR, etc.            |
-| message      | Text      | Main description             |
+| message      | String    | Main description             |
 | log_metadata | JSONB     | Flexible software-specific   |
 | timestamp    | DateTime  | Server default now           |
+| user_id      | Integer   | FK -> users(id)              |
 -----------------------------------------------------------
+
+Table: users
+--------------------------------------------------------------------
+| Column          | Type      | Note                               |
+|-----------------|-----------|------------------------------------|
+| id              | Integer   | PK, Autoincrement                  |
+| username        | String    | Indexed, e.g. 'mariogon'           |
+| email           | String    | Indexed, e.g. 'example@gmail.com   |
+| hashed_password | String    | Hash value                         |
+| is_active       | Boolean   | Default=True                       |
+--------------------------------------------------------------------
 
 """
 
 # Import column object and column types from SQLAlchemy
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
 
 # Import JSONB attribute to save metadata (Only in PostgreSQL)
 from sqlalchemy.dialects.postgresql import JSONB
@@ -36,16 +48,42 @@ class Log(Base):
     """
     Log Class from Sentinel API Database
     
-    Once the data is validated, the data mentioned in 
+    Once the log data is validated, the data mentioned in 
     the Docstring is recorded in a column of this table.
     """
     # Define table name
     __tablename__ = 'logs'
 
-    # Define attributes
+    # Define log attributes
     id = Column(Integer, primary_key=True, autoincrement=True)
     service_name = Column(String(50), index=True)
     log_level = Column(String(20), nullable=False)
     message = Column(String(1000), nullable=False)
     log_metadata = Column(JSONB, nullable=True)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
+
+    # Create foreign key that references users table
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+
+# Create users class
+class User(Base):
+    """
+    Users class from Sentinel API database
+
+    Once a user is registered, data is saved
+    in this table in the database.
+    """
+
+    # Define table name
+    __tablename__ = "users"
+
+    # Define user attributes
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, index=True, nullable=False)
+    email = Column(String(100), unique=True, index=True, nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    is_active = Column(Boolean, default=True)
+    
+    # Timestamps for auditory
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
