137dac3133cbe8cfac23dc55781e5b18719c35f1
dmgonzalezhenao <dmgh20212022@gmail.com>
2026-03-18 11:11:36 -0500

    feat: implement multi-tenant database architecture v0.5.0
    Created Organization model with unique slug-based indexing.
    
    Updated User and Log models with mandatory organization_id (FK).
    
    Established bidirectional ORM relationships using back_populates.
    
    Added automated created_at and updated_at timestamps to all entities.
    
    Enforced data integrity with nullable=False on critical foreign keys.

--- app/database/models.py
+++ app/database/models.py
@@ -1,39 +1,61 @@
 """
-Sentinel API Database classes
+Sentinel API Database Models v0.5.0
 
-This module creates and configures the structure of the database
-in Neon by using SQLAlchemy to create classes and its attributes.
-By now this is the initial structure:
+Architecture: Multi-tenant Log Aggregator.
+Engine: PostgreSQL (Neon) via SQLAlchemy ORM.
+
+Relationships:
+- Organization (1) <---> (N) Users (Admin, Viewer, Service)
+- Organization (1) <---> (N) Logs (Enforced Isolation)
+- User (1) <---> (N) Logs (Authorship traceability)
 
 Table: logs
------------------------------------------------------------
-| Column       | Type      | Note                         |
-|--------------|-----------|------------------------------|
-| id           | Integer   | PK, Autoincrement            |
-| service_name | String    | Indexed, e.g. 'web-app'      |
-| log_level    | String    | INFO, ERROR, etc.            |
-| message      | String    | Main description             |
-| log_metadata | JSONB     | Flexible software-specific   |
-| timestamp    | DateTime  | Server default now           |
-| user_id      | Integer   | FK -> users(id)              |
------------------------------------------------------------
+--------------------------------------------------------------
+| Column          | Type      | Note                         |
+|-----------------|-----------|------------------------------|
+| id              | Integer   | PK, Autoincrement            |
+| service_name    | String    | Indexed, e.g. 'web-app'      |
+| log_level       | String    | INFO, ERROR, etc.            |
+| message         | String    | Main description             |
+| log_metadata    | JSONB     | Flexible software-specific   |
+| timestamp       | DateTime  | Server default now time      |
+| user_id         | Integer   | FK -> users(id)              |
+| organization_id | Integer   | FK -> organizations(id)      |
+--------------------------------------------------------------
 
 Table: users
---------------------------------------------------------------------
-| Column          | Type      | Note                               |
-|-----------------|-----------|------------------------------------|
-| id              | Integer   | PK, Autoincrement                  |
-| username        | String    | Indexed, e.g. 'mariogon'           |
-| email           | String    | Indexed, e.g. 'example@gmail.com   |
-| hashed_password | String    | Hash value                         |
-| is_active       | Boolean   | Default=True                       |
-| role            | String    | ADMIN, SERVICE and VIEWER          |
---------------------------------------------------------------------
-
+----------------------------------------------------------------------------
+| Column          | Type      | Note                                       |
+|-----------------|-----------|--------------------------------------------|
+| id              | Integer   | PK, Autoincrement                          |
+| username        | String    | Indexed, Unique, e.g. 'mariogon'           |
+| email           | String    | Indexed, Unique, e.g. 'example@gmail.com   |
+| hashed_password | String    | Hash value                                 |
+| is_active       | Boolean   | Default=True                               |
+| role            | String    | ADMIN, SERVICE and VIEWER                  |
+| created_at      | DateTime  | Server default now time                    |
+| updated_at      | DateTime  | Server default now time                    |
+| organization_id | Integer   | FK -> organizations(id)                    |
+----------------------------------------------------------------------------
+ 
+Table: organizations
+-----------------------------------------------------------------
+| Column      | Type      | Note                                |
+-----------------------------------------------------------------
+| id          | Integer   | PK, Autoincrement                   |
+| name        | String    | Indexed, Unique, e.g. "Acme Corp"   |
+| slug        | String    | Indexed, Unique, eg.g "acme"        |
+| is_active   | Boolean   | Default=True                        |
+| created_at  | DateTime  | Server default now time             |
+| updated_at  | DateTime  | Server default now time             |
+-----------------------------------------------------------------
 """
 
 # Import column object and column types from SQLAlchemy
 from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+
+# Import relationship to optimize joins
+from sqlalchemy.orm import relationship
 
 # Import JSONB attribute to save metadata (Only in PostgreSQL)
 from sqlalchemy.dialects.postgresql import JSONB
@@ -58,13 +80,18 @@ class Log(Base):
     # Define log attributes
     id = Column(Integer, primary_key=True, autoincrement=True)
     service_name = Column(String(50), index=True)
-    log_level = Column(String(20), nullable=False)
+    log_level = Column(String(20), index=True, nullable=False)
     message = Column(String(1000), nullable=False)
     log_metadata = Column(JSONB, nullable=True)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
 
-    # Create foreign key that references users table
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+    # Create foreign keys that reference user and organization
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    organization_id = Column(Integer, ForeignKey("organizations.id"), nullable=False)
+
+    # Optimize relationships to log's organization and log's author
+    organization = relationship("Organization", back_populates="logs")
+    author = relationship("User", back_populates="logs")
 
 # Create users class
 class User(Base):
@@ -79,9 +106,9 @@ class User(Base):
     __tablename__ = "users"
 
     # Define user attributes
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)
-    email = Column(String(100), unique=True, index=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     is_active = Column(Boolean, default=True)
     role = Column(String(20), nullable=False, default="VIEWER")
@@ -89,3 +116,35 @@ class User(Base):
     # Timestamps for auditing.
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+
+    # Foreign key to organizations table
+    organization_id = Column(Integer, ForeignKey("organizations.id"), nullable=False)
+
+    # Define relationship to organization and logs as "author"
+    organization = relationship("Organization", back_populates="users")
+    logs = relationship("Log", back_populates="author")
+
+class Organization(Base):
+    """
+    Organization class from Sentinel API Database
+
+    This table allows to viewers and services to
+    view and create same logs respectively.
+    """
+
+    # Define table name
+    __tablename__ = "organizations"
+
+    # Define organization attributes
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(100), unique=True, nullable=False)
+    slug = Column(String(50), unique=True, nullable=False)
+    is_active = Column(Boolean, default=True)
+
+    # Timestamps for auditing.
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now(), server_default=func.now())
+
+    # Create relationship to optimize queries from organizations to users and logs
+    users = relationship("User", back_populates="organization")
+    logs = relationship("Log", back_populates="organization")
